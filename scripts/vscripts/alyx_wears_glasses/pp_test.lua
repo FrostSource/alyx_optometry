5e7e658139afaf32cc9773d72367eebba3ef30a4
FrostSource <jjhfrost@gmail.com>
2022-01-02 05:44:25 +1300

    Initial release.

--- scripts/vscripts/alyx_wears_glasses/pp_test.lua
+++
@@ -1,6 +0,0 @@
-
-function Precache(context)
-    print("PP PRECACHE")
-    
-    PrecacheModel("maps/glasses_meshes/entities/pp_blur_mesh_2.vmdl", context)
-end
---
+++ scripts/vscripts/mods/init/2703180455.lua
@@ -0,0 +1,2 @@
+-- Main workshop file.
+require("alyx_wears_glasses/alyx_wears_glasses_init")
